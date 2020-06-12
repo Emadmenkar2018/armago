1f681001c3f369c053a8b96444ac47d85ad40ac4
Vincenzo <thebestupwork90@gmail.com>
2020-06-12 09:38:04 +0200

    Message filtering completed

--- src/screens/Messages.js
+++ src/screens/Messages.js
@@ -1,15 +1,80 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, TextInput, SafeAreaView, TouchableOpacity,ScrollView } from 'react-native';
 import { images } from '../common/images';
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import { colors } from '../common/colors';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AppStatusBar from '../components/AppStatusBar';
+import SearchInput, { createFilter } from 'react-native-search-filter';
 export const { width, height } = Dimensions.get('window');
-
+const KEYS_TO_FILTERS_NEWMATCHES = ['name'];
+const KEYS_TO_FILTERS_MESSAGES = ['name','message'];
 export default class Messages extends Component {
+  newMatches = [
+    {
+      id : 1,
+      name : 'Jeffery',
+      avatar : images.user1
+    },
+    {
+      id : 2,
+      name : 'Alan',
+      avatar : images.user2
+    },
+    {
+      id : 3,
+      name : 'Leo',
+      avatar : images.user3
+    },
+    {
+      id : 4,
+      name : 'Chris',
+      avatar : images.user4
+    }
+  ]
+  Messages = [
+    {
+      id : 10, 
+      name : 'Mathew',
+      avatar : images.user6,
+      message: 'Still up for tonight?'
+    },
+    {
+      id : 11, 
+      name : 'Mathaw',
+      avatar : images.user7,
+      message: 'Hello, Are you there?'
+    },
+    {
+      id : 12, 
+      name : 'Matsew',
+      avatar : images.user8,
+      message: 'Hi Good morning!'
+    }
+  ]
+  constructor(props){
+    super(props);
+    this.state = {
+      
+      searchTerm : ''
+    }
+    
+  }
+  searchUpdated(term) {
+    this.setState({ searchTerm: term })
+  }
+  searchbyText(keyword){
+    
+    this.setState({
+      newMatches : this.state.newMatches.filter(match => match.name.includes('')),
+      Messages : this.state.Messages.filter(message =>  message.message.includes(''))
+    })
+    
+  }
   render() {
     const { navigate } = this.props.navigation;
+    const filtered_newMatches = this.newMatches.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS_NEWMATCHES ))
+    const filtered_Messages = this.Messages.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS_MESSAGES ))
     return (
       <>
         <AppStatusBar backgroundColor={colors.lightgreen} barStyle={Platform.OS === 'ios' ? 'dark-content':'light-content'}></AppStatusBar>
@@ -22,81 +87,63 @@ export default class Messages extends Component {
             <Image source={images.msg} style={styles.msg} />
             <AntDesign name="home" size={26} color={"white"} style={{ opacity: 0 }} />
           </View>
-          <TextInput style={styles.input} placeholder={'🔍 Search 8 matches'} />
+          {/* <TextInput 
+            style={styles.input} 
+            placeholder={'🔍 Search 8 matches'} 
+            onChangeText = {text => this.searchbyText(text)}
+          /> */}
+          <SearchInput 
+            onChangeText={(term) => { this.searchUpdated(term) }} 
+            style={styles.input}
+            placeholder={'🔍 Search 8 matches'} 
+          />
           <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 12 }}>
             <Text style={{ color: 'white', fontFamily: "ProximaNova-Bold", fontSize: 16 }}>New Matches</Text>
             <View style={styles.circle}>
-              <Text style={{ color: colors.lightgreen, fontSize: 18, fontWeight: '600' }}>5</Text>
+            <Text style={{ color: colors.lightgreen, fontSize: 18, fontWeight: '600' }}>{this.newMatches.length}</Text>
             </View>
           </View>
 
           <View style={{ flexDirection: 'row', marginTop: 12, height: 90 }} horizontal>
-            <TouchableOpacity onPress={() => navigate('Chat',{user : 'Jeffery', avatar : images.user1})}>
-              <View style={{ paddingRight: 30}}>
-                <Image source={images.user1} style={styles.user} />
-                <View style={styles.dot} />
-                <Text style={[styles.name, { marginLeft: 10 }]}>Jeffery</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigate('Chat',{user : 'Alan', avatar : images.user2})}>
-            <View style={{ paddingRight: 30 }}>
-              <Image source={images.user2} style={styles.user} />
-              <View style={styles.dot} />
-              <Text style={styles.name}>Alan</Text>
-            </View>
-            </TouchableOpacity>
-            
-            <TouchableOpacity onPress={() => navigate('Chat',{user : 'Leo', avatar : images.user3})}>
-            <View style={{ paddingRight: 30 }}>
-              <Image source={images.user3} style={styles.user} />
-              <View style={styles.dot} />
-              <Text style={styles.name}>Leo</Text>
-            </View>
-            </TouchableOpacity>
-            
-            <TouchableOpacity onPress={() => navigate('Chat',{user : 'Chris', avatar : images.user4})}>
-            <View style={{ paddingRight: 30 }}>
-              <Image source={images.user4} style={styles.user} />
-              <View style={styles.dot} />
-              <Text style={styles.name}>Chris</Text>
-            </View>
-            </TouchableOpacity>
+            {
+              filtered_newMatches.map((prop, key) => {
+                return (
+                  <TouchableOpacity key={prop.id} onPress={() => navigate('Chat',{user : prop.name, avatar : prop.avatar})}>
+                    <View style={{ paddingRight: 30}}>
+                      <Image source={prop.avatar} style={styles.user} />
+                      <View style={styles.dot} />
+                      <Text style={[styles.name, { marginLeft: 10 }]}>{prop.name}</Text>
+                    </View>
+                  </TouchableOpacity>
+                )
+              })
+            }
             
           </View>
 
           <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 12 }}>
             <Text style={{ color: colors.lightgreen, fontFamily: "ProximaNova-Bold", fontSize: 16 }}>Messages</Text>
             <View style={[styles.circle, { backgroundColor: colors.lightgreen}]}>
-              <Text style={{ color: colors.white, fontSize: 18, fontWeight: '600' }}>2</Text>
+          <Text style={{ color: colors.white, fontSize: 18, fontWeight: '600' }}>{this.Messages.length}</Text>
             </View>
           </View>
-
-          <View style={styles.list}>
-            <Image source={images.user6} style={styles.user}/>
-            <View style={[styles.dot, { left: 59}]} />
-            <View style={styles.listborder}>
-              <Text style={{ fontSize: 20, fontFamily: "ProximaNova-Bold", color: '#666'}}>Mathew</Text>
-              <Text style={{ fontSize: 16, fontFamily: 'ProximaNova-Regular', color: '#999'}}>Still up for tonight?</Text>
-            </View>
-          </View>
-
-          <View style={styles.list}>
-            <Image source={images.user7} style={styles.user}/>
-            <View style={[styles.dot, { left: 59}]} />
-            <View style={styles.listborder}>
-              <Text style={{ fontSize: 20, fontFamily: "ProximaNova-Bold", color: '#666'}}>Mathew</Text>
-              <Text style={{ fontSize: 16, fontFamily: 'ProximaNova-Regular', color: '#999'}}>Still up for tonight?</Text>
-            </View>
-          </View>
-
-          <View style={styles.list}>
-            <Image source={images.user8} style={styles.user}/>
-            <View style={[styles.dot, { left: 59}]} />
-            <View style={styles.listborder}>
-              <Text style={{ fontSize: 20, fontFamily: "ProximaNova-Bold", color: '#666'}}>Mathew</Text>
-              <Text style={{ fontSize: 16, fontFamily: 'ProximaNova-Regular', color: '#999'}}>Still up for tonight?</Text>
-            </View>
-          </View>
+          <ScrollView>
+            {
+              filtered_Messages.map((prop, key) => {
+                return (
+                  <View style={styles.list}  key={prop.id} >
+                    <Image source={prop.avatar} style={styles.user}/>
+                    <View style={[styles.dot, { left: 59}]} />
+                    <View style={styles.listborder}>
+                      <Text style={{ fontSize: 20, fontFamily: "ProximaNova-Bold", color: '#666'}}>{prop.name}</Text>
+                      <Text style={{ fontSize: 16, fontFamily: 'ProximaNova-Regular', color: '#999'}}>{prop.message}</Text>
+                    </View>
+                  </View>
+                )
+              })
+            }
+          </ScrollView>
+          
           
         </View>
         <Image source={images.oval} style={styles.oval2} />
